349362cff8ff07077d0cd42287960657c02e1727
Luan Louzada <luanlouzadadev@gmail.com>
2024-03-29 05:40:48 -0300

    feat:obsidian, telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,5 +3,19 @@ return {
   keys = {
     { "<leader><space>", LazyVim.telescope("files", { cwd = vim.fn.expand("%:p:h") }), desc = "Find Files (cwd)" },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+    {
+      "<leader>fo",
+      function()
+        require("telescope.builtin").find_files({ cwd = "~/Documents/repositories/" })
+      end,
+      desc = "Find Files in Obsidian",
+    },
+    {
+      "<leader>fg",
+      function()
+        require("telescope.builtin").live_grep({ cwd = "~/Documents/repositories" })
+      end,
+      desc = "Grep in Obsidian",
+    },
   },
 }
