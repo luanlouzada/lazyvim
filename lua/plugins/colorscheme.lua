08f279aacf1894f83f1e45fa465f12968373b467
Luan Louzada <luanlouzadadev@gmail.com>
2024-05-14 21:57:16 -0300

    commit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,41 @@
+-- colorscheme.lua
+local function get_window_class()
+  -- Obter o ID da janela ativa
+  local window_id = vim.fn.system("xdotool getactivewindow")
+  window_id = window_id:gsub("%s+", "")
+
+  -- Obter a classe da janela usando xprop
+  local window_class = vim.fn.system("xprop -id " .. window_id .. " | grep 'WM_CLASS' | awk -F '\"' '{print $4}'")
+  window_class = window_class:gsub("%s+", "")
+
+  return window_class
+end
+
+local function set_theme()
+  local window_class = get_window_class()
+
+  if window_class == "Alacritty" then
+    vim.cmd("colorscheme embark")
+  elseif window_class == "kitty" then
+    vim.cmd("colorscheme gruvbox")
+  else
+    vim.cmd("colorscheme default")
+  end
+end
+
 return {
   {
     "embark-theme/vim",
     as = "embark", -- Renomeia o plugin para 'embark'
     config = function()
-      vim.cmd("colorscheme embark") -- Configura o embark como o tema atual
+      set_theme()
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    as = "gruvbox", -- Renomeia o plugin para 'gruvbox'
+    config = function()
+      set_theme()
     end,
   },
 }
