ab73117895e99a8e22e9e28ef39348b5b41847bc
Luan Louzada <luanlouzadadev@gmail.com>
2024-05-16 23:43:40 -0300

    feat:1

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -33,9 +33,44 @@ return {
   },
   {
     "ellisonleao/gruvbox.nvim",
-    as = "gruvbox", -- Renomeia o plugin para 'gruvbox'
+    priority = 1000,
     config = function()
-      set_theme()
+      require("gruvbox").setup({
+        contrast = "hard",
+        palette_overrides = { dark0_hard = "#0E1018", bright_red = "#C6200E" },
+        overrides = {
+          Comment = { fg = "#81878f", italic = true, bold = true },
+          Define = { link = "GruvboxPurple" },
+          Macro = { link = "GruvboxPurple" },
+          ["@constant.builtin"] = { link = "GruvboxPurple" },
+          ["@storageclass.lifetime"] = { link = "GruvboxAqua" },
+          ["@text.note"] = { link = "TODO" },
+          ["@namespace.latex"] = { link = "Include" },
+          ["@namespace.rust"] = { link = "Include" },
+          ContextVt = { fg = "#878788" },
+          CopilotSuggestion = { fg = "#878787" },
+          DiagnosticVirtualTextWarn = { fg = "#dfaf87" },
+          Folded = { fg = "#fe8019", bg = "#3c3836", italic = true },
+          FoldColumn = { fg = "#fe8019", bg = "#0E1018" },
+          SignColumn = { bg = "#fe8019" },
+          DiffAdd = { bold = true, reverse = false, fg = "", bg = "#2a4333" },
+          DiffChange = { bold = true, reverse = false, fg = "", bg = "#333841" },
+          DiffDelete = { bold = true, reverse = false, fg = "#442d30", bg = "#442d30" },
+          DiffText = { bold = true, reverse = false, fg = "", bg = "#213352" },
+          StatusLine = { bg = "#ffffff", fg = "#0E1018" },
+          StatusLineNC = { bg = "#3c3836", fg = "#0E1018" },
+          CursorLineNr = { fg = "#fabd2f", bg = "" },
+          GruvboxOrangeSign = { fg = "#dfaf87", bg = "" },
+          GruvboxAquaSign = { fg = "#8EC07C", bg = "" },
+          GruvboxGreenSign = { fg = "#C6200E", bg = "" },
+          GruvboxRedSign = { fg = "#F4ACA4", bg = "" },
+          GruvboxBlueSign = { fg = "#83a598", bg = "" },
+          WilderMenu = { fg = "#ebdbb2", bg = "" },
+          WilderAccent = { fg = "#f4468f", bg = "" },
+          ["@neorg.markup.inline_macro"] = { link = "GruvboxGreen" },
+        },
+      })
+      vim.cmd("colorscheme gruvbox")
     end,
   },
 }
