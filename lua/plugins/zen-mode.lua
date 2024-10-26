33a1d84d9230bb9fa0d2e5cd988dbad153903534
Luan Louzada <luanlouzadadev@gmail.com>
2024-10-26 09:11:58 -0300

    feat:zen-mode

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,6 +1,40 @@
 return {
   {
     "folke/zen-mode.nvim",
-    opts = {},
+    opts = {
+      window = {
+        width = 120,
+        height = 1,
+        options = {
+          signcolumn = "no",
+          number = false,
+          relativenumber = false,
+        },
+      },
+      plugins = {
+        options = {
+          enabled = true,
+          laststatus = 0, -- Desativa a barra de status no Zen Mode
+        },
+        kitty = {
+          enabled = true,
+          font = "+4", -- Aumenta a fonte no Kitty em 4 pontos
+        },
+      },
+      on_open = function(win)
+        vim.fn.system("kitty @ set-spacing padding=0")
+      end,
+      on_close = function()
+        vim.fn.system("kitty @ set-spacing padding=15")
+      end,
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>z"] = { "<cmd>ZenMode<cr>", "Toggle Zen Mode" },
+      },
+    },
   },
 }
